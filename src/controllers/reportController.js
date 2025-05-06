25bee0d83a5f3fa7c18911bbf0e094ecace5c892
Lucas Zani <lucaszani.costaluz@gmail.com>
2025-05-05 21:27:17 -0300

    feat: reportController added

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,35 +1,32 @@
 const PDFDocument = require("pdfkit");
 
-const heroModel = require("../models/heroModel")
+const heroModel = require("../models/heroModel");
 
 const exportPostPDF = async (req, res) => {
     try {
-        const users = await heroModel.getAllHeroes();
+        const heroes = await heroModel.getAllHeroes();
 
         res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Disposition", "inline; filename=heroes.pdf")
+        res.setHeader("Content-Disposition", "inline; filename=heroes.pdf");
 
         const doc = new PDFDocument();
         doc.pipe(res);
 
-
-        doc.fontSize(20).text("Relatorio de Heróis", {align: "center"});
+        doc.fontSize(20).text("Relatório de Heróis", { align: "center" });
         doc.moveDown();
 
-
-        doc.fontSize(12).text("id | nome | editora", {underline: true});
+        doc.fontSize(12).text("id | nome | editora", { align:"center" ,underline: true });
         doc.moveDown(0.5);
 
-    
-        hero.forEach((hero) => {
-            doc.text(
-                `${hero.id} | ${hero.name} | ${hero.email}`
-            );
+        heroes.forEach((hero) => {
+            doc.text(`${hero.id} | ${hero.name} | ${hero.id_publisher}`), { align: "center" };
         });
 
         doc.end(); 
     } catch (error) {
-        res.status(500).json({ message: "Erro ao gerar o PDF"}); 
+        if (!res.headersSent) {
+            res.status(500).json({ message: "Erro ao gerar o PDF" });
+        }
     }
 };
 
